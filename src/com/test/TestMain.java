223597279aba8f58c514608831fc3c1fb0c2d42f
luny <421991867@qq.com>
2015-05-08 13:43:23 +0800

    Signed-off-by: luny <421991867@qq.com>

---
+++ src/com/test/TestMain.java
@@ -0,0 +1,55 @@
+package com.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.spi.LoggerFactory;
+
+public class TestMain {
+
+	static Logger log=Logger.getLogger(TestMain.class);
+	
+	static Logger log1=Logger.getLogger("mylogger1");
+	
+	public static void main(String[] args) {
+		PropertyConfigurator.configure(ClassLoader.getSystemResource("log4j.properties"));
+		
+		Appender appender = LogManager.getLoggerRepository().getRootLogger().getAppender("ServerDailyRollingFile");  
+		if(appender instanceof FileAppender)     
+		{   
+		      FileAppender fileAppender = (FileAppender)appender;   
+		      fileAppender.setFile( "bin/logs/notify." + new SimpleDateFormat("yyyy-MM-dd-HH-mm").format(new Date()) + ".log");   
+		      fileAppender.activateOptions();   
+		}  
+		
+		log.warn("123!!!中文");
+		
+		log.debug("log.debug");
+		
+		log.info("log.info");
+		
+		log.error("log.error");
+		
+		
+		log.fatal("log.fatal");
+		
+		
+		log1.warn("123!!!中文111");
+		
+		log1.debug("log.debug111");
+		
+		log1.info("log.info111");
+		
+		log1.error("log.error111");
+		
+		
+		log1.fatal("log.fatal111");
+
+	}
+
+}
