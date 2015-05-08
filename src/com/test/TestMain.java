750cd7bc304b0cbda61bacec74151b2016ea7847
luny52 <luny52@163.com>
2015-05-08 16:52:11 +0800

    ddd

--- src/com/test/TestMain.java
+++ src/com/test/TestMain.java
@@ -12,58 +12,57 @@ import org.apache.log4j.spi.LoggerFactory;
 
 public class TestMain {
 
-	static Logger log=Logger.getLogger(TestMain.class);
-	
-	static Logger log1=Logger.getLogger("mylogger1");
-	
+	static Logger log = Logger.getLogger(TestMain.class);
+
+	static Logger log1 = Logger.getLogger("mylogger1");
+
 	public static void main(String[] args) {
-		PropertyConfigurator.configure(ClassLoader.getSystemResource("log4j.properties"));
-		
-		Appender appender = LogManager.getLoggerRepository().getRootLogger().getAppender("ServerDailyRollingFile");  
-		if(appender instanceof FileAppender)     
-		{   
-		      FileAppender fileAppender = (FileAppender)appender;   
-		      fileAppender.setFile( "bin/logs/notify." + new SimpleDateFormat("yyyy-MM-dd-HH-mm").format(new Date()) + ".log");   
-		      fileAppender.activateOptions();   
-		}  
-		
+		PropertyConfigurator.configure(ClassLoader
+				.getSystemResource("log4j.properties"));
+
+		Appender appender = LogManager.getLoggerRepository().getRootLogger()
+				.getAppender("ServerDailyRollingFile");
+		if (appender instanceof FileAppender) {
+			FileAppender fileAppender = (FileAppender) appender;
+			fileAppender.setFile("bin/logs/notify."
+					+ new SimpleDateFormat("yyyy-MM-dd-HH-mm")
+							.format(new Date()) + ".log");
+			fileAppender.activateOptions();
+		}
+
 		log.warn("123!!!ÖÐÎÄ");
-		
+
 		log.debug("log.debug");
-		
+
 		log.info("log.info");
-		
+
 		log.error("log.error");
-		
-		
+
 		log.fatal("log.fatal");
-		
-		
+
 		log1.warn("123!!!ÖÐÎÄ111");
-		
+
 		log1.debug("log.debug111");
-		
+
 		log1.info("log.info111");
-		
+
 		log1.info("log.info1125551");
-		
+
 		log1.error("log.error111");
-		
-		
+
 		log1.fatal("log.fatal111");
-		
+
 		log1.fatal("log.fatal777");
-		
-		//123test ....
-		
-		//lllll]]]]
+
+		// 123test ....
+
+		// lllll]]]]
 
 	}
-	
-	private void test()
-	{
-		
-	//啦啦啦啦啦
+
+	private void test() {
+
+		// 111
 	}
 
 }
